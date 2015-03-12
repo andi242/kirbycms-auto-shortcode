400fadf5ce4b79f3601686756e56c81aedca7dbb
Andreas <spamfisch01@gmail.com>
2015-03-12 17:54:23 +0100

    Create shortcodes.php

---
+++ site/plugins/shortcodes/shortcodes.php
@@ -0,0 +1,8 @@
+<?php
+  kirbytext::$pre[] = function($kirbytext, $value) {
+	  $scodes = c::get('kirbytext.shortcodes', array());
+	  $values   = array_values($scodes);
+    $keys   = array_keys($scodes);
+  	return str_replace($keys, $values, $value);
+  }
+?>
