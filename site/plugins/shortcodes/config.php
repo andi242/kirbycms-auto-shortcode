1dbb3f25a517ba7f9ea3c8de187037993571a54f
Andreas <spamfisch01@gmail.com>
2015-03-16 09:44:21 +0100

    keep /site/config/config.php clean

---
+++ site/plugins/shortcodes/config.php
@@ -0,0 +1,20 @@
+<?php
+
+c::set('kirbytext.fontawesome', array(
+		':smile:' => '<i class="icon-emo-happy"></i>',
+		':threema:' => '<span class="fa-stack fa-lg fa-fw"><i class="fa fa-comment-o fa-stack-2x"></i><i class="fa fa-lock fa-stack-1x"></i></span>',
+		':email:' => '<i class="fa fa-envelope fa-2x fa-fw"></i>',
+		':twitter:' => '<i class="fa fa-twitter"></i>',
+		':)' => '<i class="icon-emo-happy"></i>',
+		':-)' => '<i class="icon-emo-happy"></i>',
+		';)' => '<i class="icon-emo-wink"></i>',
+		';-)' => '<i class="icon-emo-wink"></i>',
+		':-/' => '<i class="icon-emo-displeased"></i>',
+		':meh:' => '<i class="icon-emo-displeased"></i>',
+		':(' => '<i class="icon-emo-unhappy"></i>',
+		':-(' => '<i class="icon-emo-unhappy"></i>',
+		':D' => '<i class="icon-emo-laugh"></i>',
+		':-D' => '<i class="icon-emo-laugh"></i>',
+		':cam:' => '<i class="fa fa-camera"></i>',
+		':clock:' => '<i class="fa fa-clock-o"></i>',
+	));
