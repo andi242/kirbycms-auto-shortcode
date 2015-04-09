23c47eebe4309db178601beace95f2a5bba87708
Andreas <andi242@users.noreply.github.com>
2015-04-09 08:32:47 +0200

    Update config.php
    bug!

--- site/plugins/shortcodes/config.php
+++ site/plugins/shortcodes/config.php
@@ -1,6 +1,6 @@
 <?php
 
-c::set('kirbytext.fontawesome', array(
+c::set('kirbytext.shortcodes', array(
 		':smile:' => '<i class="icon-emo-happy"></i>',
 		':threema:' => '<span class="fa-stack fa-lg fa-fw"><i class="fa fa-comment-o fa-stack-2x"></i><i class="fa fa-lock fa-stack-1x"></i></span>',
 		':email:' => '<i class="fa fa-envelope fa-2x fa-fw"></i>',
